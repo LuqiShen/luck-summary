74dd2688b9bcc6b42b817c5d24c673b1b58ae07b
LuqiShen <shenlq2020@gmail.com>
2020-12-29 20:52:27 +0800

    完成第五章的脚本学习

--- learnByDoing/DOMScripting#Keith/Chapter5/script.js
+++ learnByDoing/DOMScripting#Keith/Chapter5/script.js
@@ -1,3 +1,18 @@
 function popUp(winURL) {
     window.open(winURL, "popUp", "width=320,height=480");
 }
+
+function prepareLinks() {
+    if(!document.getElementsByTagName) return false;
+    var links = document.getElementsByTagName('a');
+    for (let i = 0; i < links.length; i++) {
+        if(links[i].getAttribute("class") == "popup"){
+            links[i].onclick = function () {
+                popUp(this.getAttribute("href"));
+                return false;
+            }
+        }
+    }
+}
+
+window.onload = prepareLinks;
