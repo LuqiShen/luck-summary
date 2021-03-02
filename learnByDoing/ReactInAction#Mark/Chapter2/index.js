0585ec39e0ab334a96f053f824c4e206817893e5
LuqiShen <shenlq2020@gmail.com>
2021-03-02 23:12:06 +0800

    React/2: 2.2.1 创建React元素

--- learnByDoing/ReactInAction#Mark/Chapter2/index.js
+++ learnByDoing/ReactInAction#Mark/Chapter2/index.js
@@ -1,1 +1,13 @@
-const node = document.getElementById('root');
+// 如果将本脚本放在页面头部，则ReactDOM.render函数将会报错，因为此时DOM还没有生成id为root的div元素
+const domElement = document.getElementById("root");
+console.log(domElement);
+const reactElement = React.createElement('div', {}, 
+    React.createElement('h1', {}, "Hello World!",
+        React.createElement('a', { href: 'LuqiShen.github.io'},
+            React.createElement('h1', {}, "React In Action"),
+            React.createElement('em', {}, "... and now it really is!")
+        )
+    )
+)
+
+ReactDOM.render(reactElement, domElement);
