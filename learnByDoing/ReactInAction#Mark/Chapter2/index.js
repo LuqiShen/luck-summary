ba2910ca382f5c99e60852229da5f5623a05f665
‘lucy@LucyShendeMacBook-Air.local’ <shenlq2020@gmail.com>
2021-03-05 20:26:05 +0800

    React/2: 2.2.4 创建React类组件 PropTypes库引入失败

--- learnByDoing/ReactInAction#Mark/Chapter2/index.js
+++ learnByDoing/ReactInAction#Mark/Chapter2/index.js
@@ -1,3 +1,5 @@
+// import * as PropTypes from 'prop-types';
+
 // 如果将本脚本放在页面头部，则ReactDOM.render函数将会报错，因为此时DOM还没有生成id为root的div元素
 const domElement = document.getElementById("root");
 console.log(domElement);
@@ -11,3 +13,43 @@ const reactElement = React.createElement('div', {},
 )
 
 ReactDOM.render(reactElement, domElement);
+
+class Post extends React.Component{
+    render(){
+        return React.createElement(
+            'div',
+            {
+                className: 'post'
+            },
+            React.createElement(
+                'h2',
+                {
+                    className: 'postAuthor',
+                    id: this.props.id
+                },
+                this.props.user,
+                React.createElement(
+                    'span',
+                    {
+                        className: 'postBody'
+                    },
+                    this.props.content
+                )
+            )
+        )
+    }
+}
+
+// Post.propTypes = {
+//     user: PropTypes.string.isRequired,
+//     content: PropTypes.string.isRequired,
+//     id: PropTypes
+// }
+
+const App = React.createElement(Post, {
+    id: 1,
+    content: ' said: This is a post!',
+    user: 'mark'
+});
+
+ReactDOM.render(App, domElement);
